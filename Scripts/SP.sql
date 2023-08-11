3ac68c9aa6fadf12a2bbd56374c1c9e8de88a603
Karthik <56579449+karthik9728@users.noreply.github.com>
2023-08-12 00:33:01 +0530

    Store Procedures

---
+++ Scripts/SP.sql
@@ -0,0 +1,20 @@
+USE [SchoolManagement_DB]
+
+--EXEC [dbo].[sp_SelectAllStudents];
+
+--EXEC [dbo].[sp_SelectAllStudentById] @StudentId = 3;
+
+--EXEC [dbo].[spins_AddStudent] 
+--@studentId = '1478323620',
+--@fristName = 'Abbi',
+--@lastName ='Thompson',
+--@programmeId = 2;
+
+--DECLARE  @id INT
+
+--EXEC [dbo].[spins_AddStudent] 
+--'1278323620','Felicia','Thompson', 2,@id OUTPUT;
+
+--SELECT @id;
+
+--SELECT * FROM Students;
