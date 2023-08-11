1913b4851dce7461c58aad81f026f83f117c05e3
Karthik <56579449+karthik9728@users.noreply.github.com>
2023-08-11 18:50:25 +0530

    AVG,COUNT,MIN and MAX

--- Scripts/GroupByAndAggreateFunctions.sql
+++ Scripts/GroupByAndAggreateFunctions.sql
@@ -15,6 +15,46 @@ USE [SchoolManagement_DB]
 
 --SELECT COUNT(Id)[No of Students] FROM Students;
 
-SELECT s.StudentId,s.FirstName +' '+ s.LastName [Student Name],COUNT(e.StudentId) [Number of Classes] FROM Enrollments e
-INNER JOIN Students s ON s.Id = e.StudentId
-GROUP BY s.StudentId,s.FirstName,s.LastName;
+--SELECT s.StudentId,s.FirstName +' '+ s.LastName [Student Name],COUNT(e.StudentId) [Number of Classes] FROM Enrollments e
+--INNER JOIN Students s ON s.Id = e.StudentId
+--GROUP BY s.StudentId,s.FirstName,s.LastName;
+
+--Average
+--SELECT 
+--s.StudentId,
+--s.FirstName +' '+ s.LastName [Student Name],
+--AVG(Grade)[AVG Grade],
+--COUNT(Grade) [No Of Classes] 
+--FROM Enrollments e
+--INNER JOIN Students s ON s.Id = e.StudentId
+--GROUP BY s.StudentId,s.FirstName,s.LastName;
+
+----Min and Max
+--SELECT 
+--s.StudentId,
+--s.FirstName +' '+ s.LastName [Student Name],
+--AVG(Grade)[AVG Grade],
+--MIN(Grade)[Lowest Grade],
+--COUNT(Grade) [No Of Classes] 
+--FROM Enrollments e
+--INNER JOIN Students s ON s.Id = e.StudentId
+--GROUP BY s.StudentId,s.FirstName,s.LastName;
+
+--SELECT 
+--s.StudentId,
+--s.FirstName +' '+ s.LastName [Student Name],
+--AVG(Grade)[AVG Grade],
+--MAX(Grade)[Lowest Grade],
+--COUNT(Grade) [No Of Classes] 
+--FROM Enrollments e
+--INNER JOIN Students s ON s.Id = e.StudentId
+--GROUP BY s.StudentId,s.FirstName,s.LastName;
+
+--Select MIN(Grade) from Enrollments WHERE ClassId = 1;
+
+--SELECT 
+--COUNT(Id) [How Many Students],
+--AVG(Grade) [Average Class Grade],
+--Min(Grade) [Lowest Class Grade],
+--Max(Grade) [Higest Class Grade]
+--FROM Enrollments WHERE ClassId = 1;
